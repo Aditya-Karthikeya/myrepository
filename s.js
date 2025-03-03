45e7f2d0345e0721054fcb98b58f4e197ab60d50
Aditya-Karthikeya <ongoleaditya@gmail.com>
2025-03-03 22:20:49 +0530

    Remove unwanted files and update .gitignore

--- s.js
+++
@@ -1,8 +0,0 @@
-let nam=document.getElementById("name");
-let button=document.getElementById("btn");
-function name1(){
-    a=["Amma","Nanna","Akka"];
-    i=Math.floor(Math.random() * a.length);
-    nam.innerText=a[i];
-}
-button.addEventListener("click",name1);
